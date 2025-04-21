ceda44ffca84c2818060b9e84b997144c6e5ca06
Angie Roa <a.roap@uniandes.edu.co>
2025-04-21 07:07:49 -0500

    feat: add album components

--- app/src/main/java/com/uniandes/vinilos/data/model/Album.kt
+++ app/src/main/java/com/uniandes/vinilos/data/model/Album.kt
@@ -1,2 +1,8 @@
 package com.uniandes.vinilos.data.model
 
+data class Album(
+    val id: String,
+    val cover: String,
+    val name: String,
+    val artist: String
+)
--- app/src/main/java/com/uniandes/vinilos/ui/features/album/AlbumCreate.kt
+++ app/src/main/java/com/uniandes/vinilos/ui/features/album/AlbumCreate.kt
@@ -1,2 +1,38 @@
 package com.uniandes.vinilos.ui.features.album
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.navigation.NavHostController
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.uniandes.vinilos.ui.components.FormButtons
+import com.uniandes.vinilos.ui.components.LogoHeader
+
+@Composable
+fun AlbumCreate(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp)
+    ) {
+
+        LogoHeader("album_screen", navController = navController)
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = "Crear un nuevo álbum",
+            style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.ExtraBold),
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        FormButtons("album_screen", navController = navController)
+    }
+}
