3b04f88999ce83f0c6918bafc43856372c1666cc
Esteban Heredia <e.herediar@uniandes.edu.co>
2025-04-23 23:44:09 -0500

    ajustes en pruebas e2e

--- app/src/androidTest/java/com/uniandes/vinilos/AlbumTest.kt
+++ app/src/androidTest/java/com/uniandes/vinilos/AlbumTest.kt
@@ -1,8 +1,6 @@
 package com.uniandes.vinilos
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
 import org.junit.Rule
@@ -12,28 +10,28 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class AlbumTest {
 
-    @get:Rule
-    val rule = createAndroidComposeRule<MainActivity>()
-
+    @get:Rule val rule = createAndroidComposeRule<MainActivity>()
     private val commonSteps = CommonSteps()
+
+    @Before
+    fun resetAppState() {
+        commonSteps.enterApplication(rule)
+    }
 
     @Test
     fun showAlbumList() {
-        commonSteps.enterApplication(rule)
         commonSteps.clickOn(rule, "show_more_album")
         commonSteps.validateListIsVisible(rule, "album_list")
     }
 
     @Test
     fun showAlbumInHome() {
-        commonSteps.enterApplication(rule)
         commonSteps.searchSection(rule, "album_list")
         commonSteps.countItems(rule, "album_item")
     }
 
     @Test
     fun showAlbumDetailsFromList() {
-        commonSteps.enterApplication(rule)
         commonSteps.clickOn(rule, "show_more_album")
         commonSteps.clickFirstItem(rule, "album_item")
         commonSteps.validateElement(rule, "album_detail")
@@ -41,7 +39,6 @@ class AlbumTest {
 
     @Test
     fun showAlbumDetailsFromHome() {
-        commonSteps.enterApplication(rule)
         commonSteps.clickFirstItem(rule, "album_item")
         commonSteps.validateElement(rule, "album_detail")
     }
