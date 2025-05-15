38c70a31d474a4eadc75d91bf7a3f412c02d3cb7
Daniela Suárez <cdanisuarez@users.noreply.github.com>
2025-05-14 19:45:41 -0500

    Adds CustomDropdown component and uses in CollectorAdd

--- app/src/main/java/com/uniandes/vinilos/ui/features/collector/CollectorAdd.kt
+++ app/src/main/java/com/uniandes/vinilos/ui/features/collector/CollectorAdd.kt
@@ -1,24 +1,36 @@
 package com.uniandes.vinilos.ui.features.collector
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.navigation.NavHostController
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.uniandes.vinilos.ui.components.CustomDropdown
 import com.uniandes.vinilos.ui.components.FormButtons
 import com.uniandes.vinilos.ui.components.LogoHeader
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun CollectorAdd(collectorId: String, navController: NavHostController) {
-    fun handleOnClickCreate() {}
+    val albums = listOf("Rock", "Pop", "Jazz")
+    var selectedAlbum by remember { mutableStateOf(albums.first()) }
+
+    fun handleOnClickCreate() {
+        Log.d("Dropdown", "Opción seleccionada: $selectedAlbum")
+    }
 
     Column(
         modifier = Modifier
@@ -33,6 +45,16 @@ fun CollectorAdd(collectorId: String, navController: NavHostController) {
             text = "Agregar álbum",
             style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.ExtraBold),
             color = MaterialTheme.colorScheme.primary
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        CustomDropdown(
+           label = "Album",
+           options = albums,
+           selectedOption = selectedAlbum,
+           onOptionSelected = { selectedAlbum = it },
+           modifier = Modifier.fillMaxWidth()
         )
 
         FormButtons(
